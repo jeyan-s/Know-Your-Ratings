b180bcbe275ce56f4e1302ed93ac76939c9259e0
jeyan-s <113619105+jeyan-s@users.noreply.github.com>
2023-07-04 10:43:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,23 +11,24 @@ import CodeAbbey from './Components/CodeAbbey';
 import Header from './Components/Header';
 
 function App() {
+  const UserNames = ["jeyan_s", "smartie_jack", "jeyan_s", "Jack_01", "Jeyan_Nino", "sasireka_13", "jeyannino001", "jeyannino001"]
   return(
     <div className = "App">
       <div class = "FullGrid">
         <Header></Header>
         <div class = "PlatformGrid">
-          <LeetCode></LeetCode>
-          <CodeChef></CodeChef>
-          <CodeForces></CodeForces>
-          <AtCoder></AtCoder>
+          <LeetCode username = {UserNames[0]}></LeetCode>
+          <CodeChef username = {UserNames[1]}></CodeChef>
+          <CodeForces username = {UserNames[2]}></CodeForces>
+          <AtCoder username = {UserNames[3]}></AtCoder>
         </div>
       </div>
       <div class = "FullGrid">
         <div class = "PlatformGrid">
-          <InterviewBit></InterviewBit>
-          <SPOJ></SPOJ>
-          <GeeksForGeeks></GeeksForGeeks>
-          <CodeAbbey></CodeAbbey>
+          <InterviewBit username = {UserNames[4]}></InterviewBit>
+          <SPOJ username = {UserNames[5]}></SPOJ>
+          <GeeksForGeeks username = {UserNames[6]}></GeeksForGeeks>
+          <CodeAbbey username = {UserNames[7]}></CodeAbbey>
         </div>
       </div>
     </div>
