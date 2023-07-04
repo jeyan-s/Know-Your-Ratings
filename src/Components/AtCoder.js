f2e8547f416533c49f853a4d51ae8a8e3ad66e82
jeyan-s <113619105+jeyan-s@users.noreply.github.com>
2023-07-04 09:43:57 +0530

    Create AtCoder.js

---
+++ src/Components/AtCoder.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Platform from './Platform';
+import axios from 'axios';
+import Logo from './LogoURL'
+const AtCoder = () => {
+  const [platformData, setPlatformData] = useState({}); // State to hold platform data
+
+  useEffect(() => {
+    // Function to fetch and update the platform data
+    const fetchPlatformData = async () => {
+      try 
+      {
+        const uname = "Jack_01"
+        const data = {}
+        const response = await axios.get(`http://localhost:5000/atcoder/${uname}`);
+        data['features'] = response.data
+        data['userName'] = uname;
+        setPlatformData(data);
+      } 
+      catch (error) 
+      {
+        console.error('Error fetching platform data:', error);
+      }
+    };
+    
+    // Call the fetchPlatformData function
+    fetchPlatformData();
+  }, []);
+
+  return (
+    <div className="AtCoder">
+        <Platform logo={Logo[3]} name="AtCoder" userName={platformData.userName} features={platformData.features}></Platform>
+    </div>
+  );
+};
+
+export default AtCoder;
