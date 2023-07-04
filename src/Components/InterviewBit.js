62e238ef643400427eacacc21a11224f68443175
jeyan-s <113619105+jeyan-s@users.noreply.github.com>
2023-07-04 10:41:41 +0530

    Update InterviewBit.js

--- src/Components/InterviewBit.js
+++ src/Components/InterviewBit.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Platform from './Platform';
 import axios from 'axios';
 import Logo from './LogoURL'
-const InterviewBit = () => {
+const InterviewBit = (props) => {
   const [platformData, setPlatformData] = useState({}); // State to hold platform data
 
   useEffect(() => {
@@ -10,8 +10,8 @@ const InterviewBit = () => {
     const fetchPlatformData = async () => {
       try 
       {
-        const uname = "Jeyan_Nino"
-        const data = {}
+        const uname = props.username;
+        const data = {};
         const response = await axios.get(`http://localhost:5000/interviewbit/${uname}`);
         data['features'] = response.data
         data['userName'] = uname;
